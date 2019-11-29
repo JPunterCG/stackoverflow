df9bfd5de9b9d1fc5784b562f6eb8666f0f041b6
Jacob Punter <jacob.punter@collinsongroup.com>
2019-11-29 11:55:06 +0000

    import os

--- Salary_ML.py
+++ Salary_ML.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
